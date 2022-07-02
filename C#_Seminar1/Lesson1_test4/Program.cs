ea27ae49f9331dd46efcb6c6b6019fb47630a9c3
Vladimir <vl.ul.le@yandex.ru>
2022-07-02 23:42:49 +0400

    max of the three

--- C#_Seminar1/Lesson1_test4/Program.cs
+++ C#_Seminar1/Lesson1_test4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// max of the three
+
+int num1, num2, num3;
+int max;
+
+Console.Write("Input a first number:  ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number:  ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+max = num1;
+
+    if (num2 > max) max = num2;
+    if (num3 > max) max = num3;
+            
+Console.WriteLine("Max number: " + max);
+
