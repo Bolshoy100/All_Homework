4ba1467f4ee286e5a4ee504441b56ad44edc44b1
Vladimir <vl.ul.le@yandex.ru>
2022-07-02 23:50:21 +0400

    all even up to n

--- C#_Seminar1/Lesson1_test8/Program.cs
+++ C#_Seminar1/Lesson1_test8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// all even up to n
+
+int n, current;
+
+Console.Write("Input any number: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+current = 2;
+
+while (current <= n)
+{
+    Console.Write(current + " ");
+    current = current + 2;
+}
