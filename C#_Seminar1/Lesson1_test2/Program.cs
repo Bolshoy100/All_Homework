0605a346002387386a141f22583228e8424a5a90
Vladimir <vl.ul.le@yandex.ru>
2022-07-02 23:36:42 +0400

    max of the two

--- C#_Seminar1/Lesson1_test2/Program.cs
+++ C#_Seminar1/Lesson1_test2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// max of the two
+
+int num1, num2;
+
+Console.Write("Input a first number:  ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) Console.WriteLine("Max=" + num1);
+if (num1 < num2) Console.WriteLine("Max=" + num2);
+if (num1 == num2) Console.WriteLine("The numbers are equal!");
