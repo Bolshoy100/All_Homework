d05cdf35583c7f66423e7b6f391b9944f5ab21b3
Vladimir <vl.ul.le@yandex.ru>
2022-07-07 18:04:54 +0400

    added lesson3

---
+++ C#_Seminar3/Lesson3_test19/Program.cs
@@ -0,0 +1,14 @@
+﻿// Palindrome
+
+void Palindr(int num)
+{   
+    string str = num.ToString();
+    if ((str[0]) == (str[4]) && (str[1]) == (str[3]))
+        Console.Write(num + ": the number is a palindrome");
+    else
+        Console.Write(num + ": the number is not a palindrome");
+} 
+      
+Console.Write("Enter a five-digit number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Palindr(n1);
---
+++ C#_Seminar3/Lesson3_test23/Program.cs
@@ -0,0 +1,15 @@
+﻿// NCub
+void NCubed(int n)
+{    
+    int current = 1;
+    while (current <= n)
+        {
+        int nqub = current * current * current;
+        Console.Write(nqub + " ");
+        current++;
+        }
+}
+
+Console.Write("Input any number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+NCubed(n1);
