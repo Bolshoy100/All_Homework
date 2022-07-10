8cd5fbd156207579fad666ae8f48607400d9ba6f
Vladimir <vl.ul.le@yandex.ru>
2022-07-10 18:00:17 +0400

    added Lesson4

---
+++ C#_Seminar4/Lesson4_Test27/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumNum(int a)
+{
+    int sum = 0;
+    while (a > 0)
+    {
+        sum = sum + a % 10;
+        a = a /10 ;
+    }
+    return sum;
+}   
+    
+Console.Write("Enter any number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The sum of all digits of the number {num} is {SumNum(num)}");
---
+++ C#_Seminar4/Lesson4_Test29/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] CreateArray(int size)
+{
+    int[] newArray = new int[size];
+
+    Console.WriteLine("Creating array here:");
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i+1} element: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    return newArray;
+} 
+
+void ShowArray(int[] array)
+{ 
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+
+        Console.WriteLine();
+}
+
+Console.Write("Input size of arrey: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array2 = CreateArray(size);
+
+ShowArray(array2);
