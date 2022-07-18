9a2cd65770e59975a9236e19703977d0c30a3c64
Vladimir <vl.ul.le@yandex.ru>
2022-07-18 12:59:57 +0400

    added seminar6

---
+++ C#_seminar6/Lesson6_test41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Total numbers greater than zero
+int[] CreateArray(int size)
+{
+    int[] newArray = new int[size];
+
+    Console.WriteLine("Creating array here:");
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i+1} element: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    return newArray;
+} 
+
+void ShowArray(int[] array)
+{ 
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+
+        Console.WriteLine();
+}
+
+int PositivElementsSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) sum = sum + 1; 
+    }
+    return sum;
+}
+
+Console.Write("Input size of arrey: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(size);
+ShowArray(array);
+
+Console.WriteLine("Total numbers greater than zero are: " + PositivElementsSum(array));
---
+++ C#_seminar6/Lesson6_test43/Program.cs
@@ -0,0 +1,40 @@
+﻿// Сoordinates of the intersection of straight lines
+
+double b1; double k1; double b2; double k2;
+
+Console.Write("Input b1: ");
+b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k1: ");
+k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b2: ");
+b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k2: ");
+k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+
+Console.WriteLine($"({x}; {y})");
+
+// Здравствуйте, Павел. У меня не получилось реализовать решение путем метода.
+//Точнее не смог разобраться с выводом в консоль сразу двух переменных x и y.
+
+/*
+double CoordIntersectionLines(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+}
+
+Console.Write("Input b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(CoordIntersectionLines()($"({x}; {y})"));
+*/
+
