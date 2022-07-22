bba9bb8dd05d0037a37ae27c330510fcf6794193
Vladimir <vl.ul.le@yandex.ru>
2022-07-22 22:06:49 +0400

    added seminar7

---
+++ C#Seminar7/Lesson7Test47/Program.cs
@@ -0,0 +1,34 @@
+﻿//create an random array of real numbers
+void RealNumb2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-1000, 1000)) / 10;
+        }
+    }
+}
+
+void Show2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] newArray = new double[m, n];
+
+RealNumb2dArray(newArray);
+Show2dArray(newArray);
---
+++ C#Seminar7/Lesson7Test50/Program.cs
@@ -0,0 +1,42 @@
+﻿// Searching for a number in an array
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Input number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(rows, columns, minValue, maxValue);
+Show2dArray(myArray);
+
+Console.Write("Enter the row number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the column number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > myArray.GetLength(0) || n > myArray.GetLength(1))
+    Console.Write("The number does not exist");
+else
+    Console.Write($"The value of the element row {m} and column {n} is: {myArray[m - 1, n - 1]}");
