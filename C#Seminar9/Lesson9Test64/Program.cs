f74b65c2946542059bd758c3ef223f865ce855d3
Vladimir <vl.ul.le@yandex.ru>
2022-07-28 00:21:31 +0400

    added seminar9

---
+++ C#Seminar9/Lesson9Test64/Program.cs
@@ -0,0 +1,13 @@
+﻿//how many digits?
+
+int NumDigits(int n)
+{
+    if (n < 10)
+    {
+        return 1;
+    }
+    return (NumDigits(n / 10)) + 1;
+}
+Console.Write("Input any number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Total digits in this number is: " + NumDigits(n));
---
+++ C#Seminar9/Lesson9Test66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Sum of elements from N up to M
+
+void DigitsSum(int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"Sum of elements from n up to m is: {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    DigitsSum(m, n, sum);
+}
+
+Console.WriteLine("Enter the minimum number m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the minimum number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+DigitsSum(m, n, 0);
